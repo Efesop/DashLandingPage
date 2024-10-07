7ddf3f5789de3a6dda57e417404b0284bd7ece77
Efesop <128099342+Efesop@users.noreply.github.com>
2024-10-07 23:54:27 +0700

    Video added

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import { Button } from "./components/ui/button"
 import { Lock, Wifi, Laptop, ChevronRight } from "lucide-react"
 import { motion } from "framer-motion"
 import Image from 'next/image'
-import dynamic from 'next/dynamic';
-
-const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 
 export default function LandingPage() {
   const [scrollY, setScrollY] = useState(0)
@@ -74,15 +71,17 @@ export default function LandingPage() {
                 className="relative"
               >
                 <div className="absolute inset-0 rounded-full" />
-                <ReactPlayer
-                  url="/images/Dash.mp4"
+                <video 
                   className="rounded-xl shadow-2xl border-6 border-gray-100 m-4 w-full"
-                  width="100%"
-                  height="auto"
-                  controls={true}
-                  playing={true}
-                  loop
-                />
+                  autoPlay 
+                  loop 
+                  muted 
+                  playsInline
+                  preload="auto"
+                >
+                  <source src="/DashLandingPage/images/Dash.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
               </motion.div>
             </div>
           </div>
