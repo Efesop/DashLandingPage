07ebfc3f1324d50bbcdbf8b5f53c691ae3f185c1
Efesop <128099342+Efesop@users.noreply.github.com>
2024-10-10 14:19:06 +0700

    Asset paths

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dashnote.io' : '',
-  basePath: '',
+  }
+  /*assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dashnote.io' : '',
+  basePath: '',*/
 }
 
 module.exports = nextConfig
