b1809565a2b7e696519bc0b7dc41d798f535408a
Efesop <128099342+Efesop@users.noreply.github.com>
2026-01-09 16:34:59 +0700

    Fixing deployment issue

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Removed output: 'export' to enable dynamic API routes for Stripe integration
   images: {
     unoptimized: true,
   },
-  /*assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dashnote.io' : '',
-  basePath: '',*/
 };
 
 module.exports = nextConfig;
